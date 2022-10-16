c7a12744ae69d9ee921b8856c426309c2250a982
oskark22 <oskar.koskiranta@gmail.com>
2022-10-16 16:36:46 +0300

    Prompt user for width

--- src/main/java/ie/atu/myOwn/Shapeapp.java
+++ src/main/java/ie/atu/myOwn/Shapeapp.java
@@ -9,5 +9,9 @@ public class Shapeapp {
         Scanner input = new Scanner(System.in);
         double length = input.nextDouble();
         System.out.println("You entered: " + length);
+
+        System.out.println("Please enter the width of the rectangle: ");
+        double width = input.nextDouble();
+        System.out.println("You entered: " + width);
     }
 }
