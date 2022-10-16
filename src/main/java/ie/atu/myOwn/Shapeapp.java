2754f7c62c192751c784ff79256dcbb1f9e5c3ae
oskark22 <oskar.koskiranta@gmail.com>
2022-10-16 16:30:47 +0300

    Prompt user for length

--- src/main/java/ie/atu/myOwn/Shapeapp.java
+++ src/main/java/ie/atu/myOwn/Shapeapp.java
@@ -1,4 +1,13 @@
 package ie.atu.myOwn;
 
+import java.util.Scanner;
+
 public class Shapeapp {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter the length of the rectangle: ");
+        Scanner input = new Scanner(System.in);
+        double length = input.nextDouble();
+        System.out.println("You entered: " + length);
+    }
 }
